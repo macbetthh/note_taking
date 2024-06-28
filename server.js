225c7fe00b1b206dc22858a06154174ddb9b7a95
Beth <143623621+macbetthh@users.noreply.github.com>
2024-06-27 18:53:46 -0600

    added server.js can't get notes to show up working on that now

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// HTML Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// API Routes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('An error occurred while reading the file.');
+      }
+      res.json(JSON.parse(data)); 
+    });
+  });
+  
+
+app.post('/api/notes', (req, res) => {
+  const newNote = { ...req.body, id: uuidv4() };
+  console.log('Received new note:', req.body); 
+  fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('An error occurred while reading the file.');
+    }
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('An error occurred while writing to the file.');
+      }
+      console.log('New note saved:', newNote); 
+      res.json(newNote);
+    });
+  });
+});
+
+
+// Bonus criteria attempt to delete route
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+  fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('An error occurred while reading the file.');
+    }
+    let notes = JSON.parse(data);
+    notes = notes.filter(note => note.id !== id);
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('An error occurred while writing to the file.');
+      }
+      res.status(204).send();
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
